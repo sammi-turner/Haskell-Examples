1ecf8378bba67706fe84ee099fef4623c212d67f
Sammi Turner <ms.sammi.turner@gmail.com>
2023-04-16 17:26:19 +0100

    Update string_to_array.hs

--- string_to_array/string_to_array.hs
+++ string_to_array/string_to_array.hs
@@ -1,3 +1,15 @@
+{-
+
+Write a function to split a string and convert it into an array of words.
+
+Examples :
+
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+-}
+
 import Data.Char (isAlphaNum)
 
 stringToArray :: String -> [String]
