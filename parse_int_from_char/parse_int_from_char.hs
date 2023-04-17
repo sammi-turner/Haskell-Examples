7f9eec075bffe11ee16671a55edefd48be71f67d
Sammi Turner <ms.sammi.turner@gmail.com>
2023-04-17 14:13:29 +0100

    Update parse_int_from_char.hs

--- parse_int_from_char/parse_int_from_char.hs
+++ parse_int_from_char/parse_int_from_char.hs
@@ -1,6 +1,6 @@
 {-
 
-You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+You ask a small girl, "How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
 
 Write a program that returns the girl's age (0-9) as an integer.
 
