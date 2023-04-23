c4a49baacee56085f7bff2f1cfd8fa4722f8ee46
Sammi Turner <ms.sammi.turner@gmail.com>
2023-04-23 14:12:55 +0100

    Add files via upload

---
+++ fizz_buzz/fizz_buzz.hs
@@ -0,0 +1,8 @@
+-- Fizz Buzz in Haskell
+fizz :: Int -> String
+fizz n | n `mod` 15 == 0  = "FizzBuzz"
+       | n `mod` 3  == 0  = "Fizz"
+       | n `mod` 5  == 0  = "Buzz"
+       | otherwise        = show n
+
+main = mapM_ (putStrLn.fizz) [1..100]
